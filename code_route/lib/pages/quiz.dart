a39d2232a4c06289c76b3247f286d6c9b5200c4a
Malek4522 <163670929+Malek4522@users.noreply.github.com>
2024-03-17 21:57:57 +0100

    malek
    quiz simple no figma ui

--- code_route/lib/pages/quiz.dart
+++ code_route/lib/pages/quiz.dart
@@ -1,5 +1,7 @@
 import 'package:code_route/classes/question.dart';
+import 'package:code_route/util/option.dart';
 import 'package:flutter/material.dart';
+import 'package:quickalert/quickalert.dart';
 
 class quiz extends StatefulWidget {
   const quiz({super.key});
@@ -10,6 +12,8 @@ class quiz extends StatefulWidget {
 
 class _quizState extends State<quiz> {
   int i = 0;
+  int score = 0;
+  bool check = false;
   List <question> mylist =[
     question(
       title: '2+2',
@@ -20,25 +24,10 @@ class _quizState extends State<quiz> {
       }
     ),
     question(
-      title: '2*3',
-      option: {
-        '6': true,
-        '7': false
-      }
-    ),
-    question(
       title: '8+2 = 10',
       option: {
         'no': false,
         'yes': true,
-      }
-    ),
-    question(
-      title: '2+2',
-      option: {
-        '3': false,
-        '4': true,
-        '5': false
       }
     ),
     question(
@@ -52,32 +41,68 @@ class _quizState extends State<quiz> {
     ),
 
   ];
+  
+  void check_fct(){
+    setState(() {
+      check = true;
+    });
+  }
+
+  void score_fct(){
+    score++;
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.indigo[800],
       appBar: AppBar(
         title: Text("question ${i+1}"),
         centerTitle: true,
       ),
-
+      
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(mylist[i].title)
+            Image.asset('assets/1.png',scale: 3),
+            Text(mylist[i].title),
+            for(String key in mylist[i].option.keys)
+              option(
+                title: key,
+                valid: mylist[i].option[key] as bool,
+                check: check,
+                checking: check_fct,
+                score: score_fct,
+              )
+
           ],
         ),
       ),
+      
 
+      
 
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton: GestureDetector(
-        child: Text("Next"),
+        child: Text("Next",style: TextStyle(backgroundColor: Colors.white),),
         onTap: () {
           if(i < mylist.length -1){
-            setState(() {
-              i++;
-            });
+            if(check){
+              setState(() {
+                i++;
+                check = false;
+              });
+            }
+          }
+          else{
+            QuickAlert.show(
+              context: context, 
+              type: QuickAlertType.success,
+              title: "You have finished your test",
+              text: "your score is: $score",
+
+            );
           }
         },
       ),
