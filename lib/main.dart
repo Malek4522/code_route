fbf0194c01df9e40c1564924bad260f11d9ab36b
Malek4522 <163670929+Malek4522@users.noreply.github.com>
2024-03-23 20:17:27 +0100

    dsq
    aa

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,11 @@
 import 'package:code_route/pages/general%20_knowledge.dart';
 import 'package:flutter/material.dart';
-// chiraz
-
-
-
 
 void main(){
   
   runApp(const Myapp());
 }
-//chiraaz
+
 
 class Myapp extends StatelessWidget {
   const Myapp({super.key});
--- lib/pages/general _knowledge.dart
+++ lib/pages/general _knowledge.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class generalknowledge extends StatelessWidget {
@@ -6,39 +8,47 @@ class generalknowledge extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue[900],
-      body: ListView.builder(
-        itemCount: 10,
-        itemBuilder: (context,index) => ListTile(
-          leading: Text(
-            (index+1).toString(),
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 20
-            ),
+      
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/backround.jpg"),          
+            fit: BoxFit.fill,
           ),
-          title:Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              color: Colors.white
+        ),
+        child: ListView.builder(
+          itemCount: 10,
+          itemBuilder: (context,index) => ListTile(
+            leading: Text(
+              (index+1).toString(),
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20
+              ),
             ),
-            child: Padding(
-              padding: EdgeInsets.all(25),
-              child: Text('title number ${index+1}')
+            title:Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(25),
+                child: Text('title number ${index+1}')
+              ),
             ),
-          ),
-          /*
-          trailing:Hero(
-            tag: "tag${index+1}", 
-            child:GestureDetector(
-              onTap: () {
-                  
-              },
-              
-            )
-          ),
-          */
-        )
+            /*
+            trailing:Hero(
+              tag: "tag${index+1}", 
+              child:GestureDetector(
+                onTap: () {
+                    
+                },
+                
+              )
+            ),
+            */
+          )
+        ),
       )
     );
   }
