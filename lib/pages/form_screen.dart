7623d4646aa6dd7a3ca8211de22fb4dcfd9fccaf
Malek4522 <163670929+Malek4522@users.noreply.github.com>
2024-03-25 21:12:11 +0100

    dfg
    cv

--- lib/pages/form_screen.dart
+++ lib/pages/form_screen.dart
@@ -9,7 +9,8 @@ class FormScreen extends StatefulWidget {
 } 
 
 class _FormScreenState extends State<FormScreen> {
-  
+  String valueChoose = "Condidat";
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -24,22 +25,24 @@ class _FormScreenState extends State<FormScreen> {
               ],
             ),
         body: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 40),
+          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 3),
           width: double.infinity,
           height: 750,
           decoration : const BoxDecoration(
             image: DecorationImage(
               image: AssetImage("assets/backround.jpg"),
               fit: BoxFit.fill,
-          )
-         ),
+           )
+           ),
+          
+             
          
-         child: Column(
+          child: Column(
           children: [
             const SizedBox(
-              height: 100,
+              height: 70,
             ),
-             Container(
+            Container(
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(16),
@@ -61,12 +64,42 @@ class _FormScreenState extends State<FormScreen> {
                   )
                 ),
               ),
-
+              ),const SizedBox(
+              height: 15,
+            ),
+           
+            Container(
+             
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+                
+              ),
+              child: const TextField(
+                 
+                decoration: InputDecoration(
+                  contentPadding:
+                     EdgeInsets.symmetric(vertical: 10),
+                  border: InputBorder.none,
+                  hintText: 'E-mail...',
+                  prefixIcon: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    child:  Icon(
+                      Icons.email, 
+                      color: Colors.black, 
+                              
+                    ),
+                
+                  )
+                ),
+                
+              ),
+              
+              
             ),
               const SizedBox(
-              height: 100,
+              height: 15,
             ),
-
             Container(
               
               decoration: BoxDecoration(
@@ -79,23 +112,126 @@ class _FormScreenState extends State<FormScreen> {
                   contentPadding:
                      EdgeInsets.symmetric(vertical: 10),
                   border: InputBorder.none,
-                  hintText: 'Email...',
+                  hintText: 'Numero de telephone...',
                   prefixIcon: Padding(
                     padding: EdgeInsets.symmetric(horizontal: 10),
                     child:  Icon(
-                      Icons.email, 
-                      color: Colors.white, 
+                      Icons.phone, 
+                      color: Colors.black, 
                               
                     ),
                   )
                 ),
               ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Container(
+              
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+                
+              ),
+              child: const TextField(
+                
+                decoration: InputDecoration(
+                  contentPadding:
+                     EdgeInsets.symmetric(vertical: 10),
+                  border: InputBorder.none,
+                  hintText: 'Mot de passe...',
+                  prefixIcon: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                    
+                   child:  Icon(
+                      Icons.lock, 
+                      color: Colors.black, 
+                      ),
+                  
+                  ),
+                  
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Container(
+              
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+                
+              ),
+              child: const TextField(
+                decoration: InputDecoration(
+                  contentPadding:
+                     EdgeInsets.symmetric(vertical: 10),
+                  border: InputBorder.none,
+                  hintText: 'Confirmer le mot de passe...',
+                  prefixIcon: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 10),
+                     child:  Icon(
+                      Icons.lock, 
+                      color: Colors.black, 
+                              
+                   ),
+                  )
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+                
+              ),
+              child:  DropdownButton  (
+                hint: const Text("Je suis un  "),
+                dropdownColor: Colors.white,
+                icon: const Icon(Icons.arrow_drop_down),
+                iconSize: 36,
+                isExpanded: true,
+                underline: const SizedBox(),
+                style: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 22
+                ),
+                value: valueChoose,
+                onChanged: (newValue) {
+                  setState(() {
+                    valueChoose = newValue!;
+                  });
+                },
+                items: const[
+                  DropdownMenuItem<String>(
+                   value: 'Condidat',
+                   child: Text('Condidat'),
+                  ),
+                  DropdownMenuItem<String>(
+                   value: 'Moniteur',
+                   child: Text('Moniteur'),
+                  ),
+                ],
+                
+               ),
+
             )
+           
+
+
+
+
 
          ],
          ),
          ),
-      ),
+         ),
+    
     
 
       
@@ -107,3 +243,4 @@ class _FormScreenState extends State<FormScreen> {
    
   }
 }
+  
