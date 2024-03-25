bc521d36aa1414395e8dcbf196114a259c6e2fa0
Malek4522 <163670929+Malek4522@users.noreply.github.com>
2024-03-25 18:54:30 +0100

    sz
    ze

--- lib/classes/auth.dart
+++ lib/classes/auth.dart
@@ -1,13 +1,19 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:code_route/classes/myuser.dart';
 
 class authservice{
   final FirebaseAuth auth = FirebaseAuth.instance;
-
+  
+  myUser? userfromfirebase(User? user){
+    return user != null? myUser(uid: user.uid):null  ;
+  }
+  
+  
   Future sign_anonym()async{
     try{
       UserCredential result =  await auth.signInAnonymously();
       User? user = result.user  ;
-      return user ;
+      return userfromfirebase(user);
     }
     catch(e){
       print("error in sign_anonym $e");
